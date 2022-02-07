ad58046a2ad6fc06256c680b6c30088a4f14d829
MSaadSheikh <95358942+MSaadSheikh@users.noreply.github.com>
2022-02-07 04:54:07 -0800

    Add files via upload

---
+++ classassignment2.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+//   List p=[1,2,3,4,"dell","laptop","hp"];
+//   stdout.write("1) Add 1 To Add Value : ");
+// var p1=stdin.readLineSync()!;
+// if (p1=="1") {
+//   stdout.write("Add New Value : ");
+//   var dt=stdin.readLineSync()!;
+//   p.add(dt);
+//   print(p);
+// }
+
+// stdout.write("2) Add 2 To Removelast Value : ");
+// var p2=stdin.readLineSync()!;
+// if (p2=="2") {
+//   p.removeLast();
+//   print(p);
+// }
+
+// stdout.write("3) Add 3 To Remove Value : ");
+// var p3=stdin.readLineSync()!;
+// if (p3=="3") {
+// p.remove(1);
+// print(p);
+// }
+
+// stdout.write("4) Add 4 To Clear All Value : ");
+// var p4=stdin.readLineSync()!;
+// if (p4=="4") {
+// p.clear();
+// print(p);
+// }
+
+// }
